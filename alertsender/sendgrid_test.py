ca0b7f52aeed213d13635df8ca09db2c59f87d22
Adam Derylo <a.m.derylo@gmail.com>
2023-01-02 22:40:26 +0100

    Create test for sendgrid api
    Signed-off-by: Adam Derylo <a.m.derylo@gmail.com>

---
+++ alertsender/sendgrid_test.py
@@ -0,0 +1,29 @@
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+import unittest
+from dotenv import load_dotenv
+
+
+class TestSendGridSetup(unittest.TestCase):
+    def setUp(self):
+        load_dotenv()
+        self.sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
+        self.sender = os.environ.get("SENDER_EMAIL")
+        self.message = Mail(
+            from_email=self.sender,
+            to_emails=self.sender,
+            subject="Test",
+            html_content="Testing sendgrid api config.",
+        )
+
+    def test_sendgrid_api_config(self):
+        try:
+            response = self.sg.send(self.message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print(e)
+
+        self.assertEqual(response.status_code, 202)
