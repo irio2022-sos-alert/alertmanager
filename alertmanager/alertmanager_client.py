7a0d5d4e31ca255cd4cfae8e108b51210c2a26e3
Adam Derylo <a.m.derylo@gmail.com>
2023-01-17 10:34:49 +0100

    add alert requeset to client

--- alertmanager/alertmanager_client.py
+++ alertmanager/alertmanager_client.py
@@ -12,12 +12,26 @@ def create_receipt_confirmation(service_id: int) -> alert_pb2.ReceiptConfirmatio
     return alert_pb2.ReceiptConfirmation(serviceId=service_id)
 
 
-def send_confirmation_test(stub: alert_pb2_grpc.AlertManagerStub):
+def create_alert_request(service_id: int) -> alert_pb2.AlertRequest:
+    return alert_pb2.AlertRequest(serviceId=service_id)
+
+
+def test_send_confirmation(stub: alert_pb2_grpc.AlertManagerStub):
     confirmation = create_receipt_confirmation(20)
     response = stub.handleReceiptConfirmation(confirmation)
 
     if response.okay:
-        print("Confirmed")
+        print("Confirmed!")
+    else:
+        print(response.message)
+
+
+def test_send_alert_request(stub: alert_pb2_grpc.AlertManagerStub):
+    request = create_alert_request(20)
+    response = stub.handleReceiptConfirmation(request)
+
+    if response.okay:
+        print("Request handled!")
     else:
         print(response.message)
 
@@ -25,7 +39,8 @@ def send_confirmation_test(stub: alert_pb2_grpc.AlertManagerStub):
 def run():
     with grpc.insecure_channel("localhost:50052") as channel:
         stub = alert_pb2_grpc.AlertManagerStub(channel)
-        send_confirmation_test(stub)
+        test_send_confirmation(stub)
+        test_send_alert_request(stub)
 
 
 if __name__ == "__main__":
