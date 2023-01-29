93751184fbddfc5bd08492196a0b64ac0b30c993
Adam Derylo <a.m.derylo@gmail.com>
2023-01-29 16:48:58 +0100

    add db connecxtion

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,17 @@
 import pytest
+from db import init_connection_pool, migrate_db
+from sqlmodel import Session
 
 
 @pytest.fixture
 def input_value():
     input = 36
     return input
+
+
+@pytest.fixture
+def session():
+    pool = init_connection_pool()
+    migrate_db(pool)
+    with Session(pool) as session:
+        yield session
--- tests/load_test.py
+++ tests/load_test.py
@@ -1,4 +1,6 @@
 import pytest
+from models import Services
+from sqlmodel import Session
 
 
 def test_divisible_by_3(input_value):
@@ -7,3 +9,8 @@ def test_divisible_by_3(input_value):
 
 def test_divisible_by_6(input_value):
     assert input_value % 6 == 0
+
+
+def test_db(session: Session):
+    services = session.query(Services).all()
+    assert len(services) == 5
